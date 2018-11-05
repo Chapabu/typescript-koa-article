1c725e850914c77c90832fa1873397f34da52353
Matt Chapman <chapabu101@gmail.com>
2018-11-05 23:52:32 +0000

    Added POST and PATCH requests.

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,7 +1,8 @@
 import * as Koa from 'koa';
 import * as Router from 'koa-router';
-import { getRepository } from 'typeorm';
-import movieEntity from '../entities/movie.entity';
+import { getRepository, Repository } from 'typeorm';
+import movieEntity from './movie.entity';
+import * as HttpStatus from 'http-status-codes';
 
 const routerOpts: Router.IRouterOptions = {
   prefix: '/movies',
@@ -9,24 +10,61 @@ const routerOpts: Router.IRouterOptions = {
 
 const router: Router = new Router(routerOpts);
 
-router.get('/', (ctx:Koa.Context, next:Koa.Middleware) => {
-  ctx.body = 'GET: MOVIE INDEX';
+router.get('/', async (ctx:Koa.Context) => {
+  const movieRepo:Repository<movieEntity> = getRepository(movieEntity);
+
+  const movies = await movieRepo.find();
+
+  ctx.body = {
+    data: { movies },
+  };
 });
 
-router.get('/:movie_id', (ctx:Koa.Context, next:Koa.Middleware) => {
-  ctx.body = `GET: SINGLE MOVIE (${ctx.params.movie_id})`;
+router.get('/:movie_id', async (ctx:Koa.Context) => {
+  const movieRepo:Repository<movieEntity> = getRepository(movieEntity);
+
+  const movie = await movieRepo.findOne(ctx.params.movie_id);
+
+  if (!movie) {
+    ctx.throw(HttpStatus.NOT_FOUND);
+  }
+
+  ctx.body = {
+    data: { movie },
+  };
 });
 
-router.post('/', (ctx:Koa.Context, next:Koa.Middleware) => {
-  ctx.body = 'POST: SINGLE MOVIE';
+router.post('/', async (ctx:Koa.Context) => {
+  const movieRepo:Repository<movieEntity> = getRepository(movieEntity);
+
+  const movie: movieEntity = movieRepo.create(ctx.request.body);
+  console.log(movie);
+  await movieRepo.save(movie);
+  ctx.status = HttpStatus.CREATED;
+  ctx.body = {
+    data: { movie },
+  };
 });
 
-router.delete('/:movie_id', (ctx:Koa.Context, next:Koa.Middleware) => {
+router.delete('/:movie_id', (ctx:Koa.Context) => {
   ctx.body = `DELETE: SINGLE MOVIE (${ctx.params.movie_id})`;
 });
 
-router.patch('/:movie_id', (ctx:Koa.Context, next:Koa.Middleware) => {
-  ctx.body = `DELETE: SINGLE MOVIE (${ctx.params.movie_id})`;
+router.patch('/:movie_id', async (ctx:Koa.Context) => {
+  const movieRepo:Repository<movieEntity> = getRepository(movieEntity);
+
+  const movie:movieEntity = await movieRepo.findOne(ctx.params.movie_id);
+
+  if (!movie) {
+    ctx.throw(HttpStatus.NOT_FOUND, 'Not found');
+  }
+
+  const updatedMovie = await movieRepo.merge(movie, ctx.request.body);
+  movieRepo.save(updatedMovie);
+
+  ctx.body = {
+    data: { movie: updatedMovie },
+  };
 });
 
 export default router;
