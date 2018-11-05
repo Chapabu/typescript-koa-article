224ea6d03a6ae5057e14a126c3439d30559bd23c
Matt Chapman <chapabu101@gmail.com>
2018-11-05 23:51:26 +0000

    Added body parser and error handler.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,30 @@
 import * as Koa from 'koa';
+import * as HttpStatus from 'http-status-codes';
+import * as bodyParser from 'koa-bodyparser';
+
 import movieController from '../movie/movie.controller';
 
 const app:Koa = new Koa();
 
+// Generic error handling middleware.
+app.use(async (ctx: Koa.Context, next: () => Promise<any>) => {
+  try {
+    await next();
+  } catch (error) {
+    ctx.status = error.statusCode || error.status || HttpStatus.INTERNAL_SERVER_ERROR;
+    ctx.body = { error };
+    ctx.app.emit('error', error, ctx);
+  }
+});
+
+// Middleware
+app.use(bodyParser());
+
+// Route middleware.
 app.use(movieController.routes());
 app.use(movieController.allowedMethods());
+
+// Application error logging.
+app.on('error', console.error);
 
 export default app;
